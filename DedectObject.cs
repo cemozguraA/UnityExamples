a960772848d554e0dcf2f0c71d284056302bfd01
cemozguraA <55392106+cemozguraA@users.noreply.github.com>
2020-08-01 16:13:19 +0300

    Add files via upload
    detects the object in the center of the screen.

---
+++ DedectObject.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class DedectObject
+{
+    public static T DedectScreenCenterObjectClass<T>() where T : MonoBehaviour
+    {
+        T TurnObject = null;
+        int x = Screen.width / 2;
+        int y = Screen.height / 2;
+
+        Ray ray = Camera.main.ScreenPointToRay(new Vector3(x, y));
+
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+            TurnObject = hit.collider.GetComponent<T>();
+
+        return TurnObject;
+
+    }
+    public static GameObject DedectScreenCenterObject()
+    {
+        GameObject TurnObject = null;
+        int x = Screen.width / 2;
+        int y = Screen.height / 2;
+
+        Ray ray = Camera.main.ScreenPointToRay(new Vector3(x, y));
+
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            TurnObject = hit.collider.gameObject;
+        }
+            
+
+        return TurnObject;
+
+    }
+
+}
